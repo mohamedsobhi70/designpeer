e97bd917eb78e9d6a143237ac3e93bf5a57e9868
mohamedsobhi70 <sobhi7070@gmail.com>
2024-07-24 16:47:40 +0300

    fix border issue

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,22 +1,27 @@
-AOS.init();
-window.addEventListener('scroll', function () {
-  const header = document.getElementById('header');
-  if (window.scrollY > 450) {
-    header.classList.add('stky');
-  } else {
-    header.classList.remove('stky');
-  }
-});
 
-document.querySelector(".menu-close-btn").addEventListener("click", () => {
-  document.querySelector(".mobile-menu").classList.toggle("hidden");
-  document.querySelector(".mobile-menu").classList.toggle("flex");
-})
-document.querySelector(".mobile-menu-btn ").addEventListener("click", () => {
-  document.querySelector(".mobile-menu").classList.toggle("hidden");
-  document.querySelector(".mobile-menu").classList.toggle("flex");
+if ($("#header").length > 0) {
+  window.addEventListener('scroll', function () {
+    const header = document.getElementById('header');
+    if (window.scrollY > 450) {
+      header.classList.add('stky');
+    } else {
+      header.classList.remove('stky');
+    }
+  });
 
-})
+}
+if ($(".mobile-menu").length > 0) {
+
+  document.querySelector(".menu-close-btn").addEventListener("click", () => {
+    document.querySelector(".mobile-menu").classList.toggle("hidden");
+    document.querySelector(".mobile-menu").classList.toggle("flex");
+  })
+  document.querySelector(".mobile-menu-btn ").addEventListener("click", () => {
+    document.querySelector(".mobile-menu").classList.toggle("hidden");
+    document.querySelector(".mobile-menu").classList.toggle("flex");
+
+  })
+}
 
 //////////////////
 ////////////////////////////////////////////
@@ -25,25 +30,27 @@ document.querySelector(".mobile-menu-btn ").addEventListener("click", () => {
 /////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////
 /////////////////
+if ($("#dark-mode-btn").length > 0) {
 
-let darkBtn = document.getElementById("dark-mode-btn");
+  let darkBtn = document.getElementById("dark-mode-btn");
 
-// Function to toggle dark mode and save preference
-function toggleDarkMode() {
-  document.documentElement.classList.toggle("dark");
-  let isDarkMode = document.documentElement.classList.contains("dark");
-  localStorage.setItem("theme", isDarkMode ? "dark" : "light");
-}
+  // Function to toggle dark mode and save preference
+  function toggleDarkMode() {
+    document.documentElement.classList.toggle("dark");
+    let isDarkMode = document.documentElement.classList.contains("dark");
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
+  }
 
-// Add event listener to the button
-darkBtn.addEventListener("click", toggleDarkMode);
+  // Add event listener to the button
+  darkBtn.addEventListener("click", toggleDarkMode);
 
-// Load the theme preference from localStorage on page load
-let savedTheme = localStorage.getItem("theme");
-if (savedTheme === "dark") {
-  document.documentElement.classList.add("dark");
-} else {
-  document.documentElement.classList.remove("dark");
+  // Load the theme preference from localStorage on page load
+  let savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
 }
 
 
@@ -76,134 +83,142 @@ window.addEventListener('scroll', () => {
 });
 
 
-
-$('.clients-carousel').owlCarousel({
-  center: true,
-  loop: true,
-  nav: false,
-  dots: false,
-  margin: 64,
-  items: 3,
-  autoplay: true,
-  autoplayTimeout: 5000,
-  autoplayHoverPause: true,
-  responsive: {
-    500: {
-      items: 4
-    },
-    992: {
-      items: 5
-    },
-    1024: {
-      items: 7
+if ($(".clients-carousel").length > 0) {
+  $('.clients-carousel').owlCarousel({
+    center: true,
+    loop: true,
+    nav: false,
+    dots: false,
+    margin: 64,
+    items: 3,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      500: {
+        items: 4
+      },
+      992: {
+        items: 5
+      },
+      1024: {
+        items: 7
+      }
     }
-  }
-});
-
-$('.work-carousel1').owlCarousel({
-  center: true,
-  nav: false,
-  dots: false,
-  mouseDrag: false,
-  touchDrag: false,
-  touchDrag: false,
-  margin: 24,
-  items: 1.3,
-  autoplay: true,
-  autoplayTimeout: 8000,
-  autoplayHoverPause: false,
-  responsive: {
-    500: {
-      items: 2
-    },
-    992: {
-      items: 3.5,
-    },
-    1024: {
-      margin: 30,
-      items: 4
-    },
-    1450: {
-      items: 4.5
-    }
-  }
-});
-
-$('.work-carousel2').owlCarousel({
-  center: true,
-  nav: false,
-  dots: false,
-  margin: 24,
-  items: 1.3,
-  autoplay: true,
-  autoplayTimeout: 8000,
-  autoplayHoverPause: false,
-  onInitialized: createCustomDots, // Call function to create custom dots
-  onChanged: function (event) {
-    syncDots(event);
-    // Check if the last slide is reached
-    let carousel = event.relatedTarget;
-    if (carousel.current() === carousel.items().length - 1) {
-      setTimeout(function () {
-        // Trigger navigation to the first slide
-        carousel.to(0, 300); // 300ms transition
-      }, 8000); // Delay to match the autoplay timeout
-    }
-  },
-  responsive: {
-    500: {
-      items: 2
-    },
-    992: {
-      items: 3.5,
-    },
-    1024: {
-      margin: 30,
-      items: 4
-    },
-    1450: {
-      items: 4.5
-    }
-  }
-})
-
-
-// Function to create custom dots
-function createCustomDots(event) {
-  let dotContainer = $("#customDots");
-  let dotCount = event.item.count;
-  dotContainer.empty(); // Clear existing dots
-  for (let i = 0; i < dotCount; i++) {
-    dotContainer.append('<span class="custom-dot" data-dot="' + i + '"></span>');
-  }
-  syncDots(event); // Sync dots with the current state
+  });
 }
 
-// Function to sync dots with the current state of the sliders
-function syncDots(event) {
-  let index = event.item.index - event.relatedTarget._clones.length / 2;
-  if (index > event.item.count) {
-    index = index - event.item.count;
-  }
-  $("#customDots .custom-dot").removeClass("active");
-  $("#customDots .custom-dot[data-dot='" + index + "']").addClass("active");
 
-  // Move both sliders
-  $('.work-carousel1').trigger("to.owl.carousel", [index, 100]);
-  $('.work-carousel2').trigger("to.owl.carousel", [index, 100]);
+if ($(".work-carousel1").length > 0) {
+
+  $('.work-carousel1').owlCarousel({
+    center: true,
+    nav: false,
+    dots: false,
+    mouseDrag: false,
+    touchDrag: false,
+    touchDrag: false,
+    margin: 24,
+    items: 1.3,
+    autoplay: true,
+    autoplayTimeout: 8000,
+    autoplayHoverPause: false,
+    responsive: {
+      500: {
+        items: 2
+      },
+      992: {
+        items: 3.5,
+      },
+      1024: {
+        margin: 30,
+        items: 4
+      },
+      1450: {
+        items: 4.5
+      }
+    }
+  });
 }
 
-// Click event for custom dots
-$("#customDots").on("click", ".custom-dot", function () {
-  let dotIndex = $(this).data("dot");
-  $('.work-carousel1').trigger("to.owl.carousel", [dotIndex, 100]);
-  $('.work-carousel2').trigger("to.owl.carousel", [dotIndex, 100]);
-});
 
-VanillaTilt.init(document.querySelector("#work-2 .center"), {
-  max: 1,
-  speed: 400,
-});
+if ($(".work-carousel2").length > 0) {
+
+  $('.work-carousel2').owlCarousel({
+    center: true,
+    nav: false,
+    dots: false,
+    margin: 24,
+    items: 1.3,
+    autoplay: true,
+    autoplayTimeout: 8000,
+    autoplayHoverPause: false,
+    onInitialized: createCustomDots, // Call function to create custom dots
+    onChanged: function (event) {
+      syncDots(event);
+      // Check if the last slide is reached
+      let carousel = event.relatedTarget;
+      if (carousel.current() === carousel.items().length - 1) {
+        setTimeout(function () {
+          // Trigger navigation to the first slide
+          carousel.to(0, 300); // 300ms transition
+        }, 8000); // Delay to match the autoplay timeout
+      }
+    },
+    responsive: {
+      500: {
+        items: 2
+      },
+      992: {
+        items: 3.5,
+      },
+      1024: {
+        margin: 30,
+        items: 4
+      },
+      1450: {
+        items: 4.5
+      }
+    }
+  })
+  // Function to create custom dots
+  function createCustomDots(event) {
+    let dotContainer = $("#customDots");
+    let dotCount = event.item.count;
+    dotContainer.empty(); // Clear existing dots
+    for (let i = 0; i < dotCount; i++) {
+      dotContainer.append('<span class="custom-dot" data-dot="' + i + '"></span>');
+    }
+    syncDots(event); // Sync dots with the current state
+  }
+
+  // Function to sync dots with the current state of the sliders
+  function syncDots(event) {
+    let index = event.item.index - event.relatedTarget._clones.length / 2;
+    if (index > event.item.count) {
+      index = index - event.item.count;
+    }
+    $("#customDots .custom-dot").removeClass("active");
+    $("#customDots .custom-dot[data-dot='" + index + "']").addClass("active");
+
+    // Move both sliders
+    $('.work-carousel1').trigger("to.owl.carousel", [index, 100]);
+    $('.work-carousel2').trigger("to.owl.carousel", [index, 100]);
+  }
+
+  // Click event for custom dots
+  $("#customDots").on("click", ".custom-dot", function () {
+    let dotIndex = $(this).data("dot");
+    $('.work-carousel1').trigger("to.owl.carousel", [dotIndex, 100]);
+    $('.work-carousel2').trigger("to.owl.carousel", [dotIndex, 100]);
+  });
+
+  VanillaTilt.init(document.querySelector("#work-2 .center"), {
+    max: 1,
+    speed: 400,
+  });
+}
+
 
 
 
@@ -256,6 +271,7 @@ gsap.matchMedia().add("(min-width: 1024px)", () => {
       scrub: true
     }
   });
+
   gsap.set(".pricing-item", { scale: .5, opacity: 0, y: 50 });
   gsap.to(".pricing-item", {
     scale: 1,
@@ -270,6 +286,7 @@ gsap.matchMedia().add("(min-width: 1024px)", () => {
       scrub: true
     }
   });
+
   gsap.set(".choice-1", { scale: .5, opacity: 0, y: 50 });
   gsap.to(".choice-1", {
     scale: 1,
@@ -312,6 +329,7 @@ gsap.matchMedia().add("(min-width: 1024px)", () => {
       scrub: true
     }
   });
+
   gsap.set(".del-item:nth-child(odd)  *", { opacity: 0, x: -100 });
   gsap.to(".del-item:nth-child(odd)  *", {
     opacity: 1,
@@ -329,31 +347,28 @@ gsap.matchMedia().add("(min-width: 1024px)", () => {
 });
 
 
-gsap.from(".deslog1", { duration: .7, opacity: 0, scale: .1, transformOrigin: "0% 50%", x: 200 })
-gsap.from(".deslog2", { duration: .5, opacity: 0, scale: .2, x: 40, delay: .5, y: 60 })
-gsap.from(".path1", { duration: .07, opacity: 0, stagger: .05, scale: 2, transformOrigin: "50% 50%", delay: 1.2 })
+// Odometer Effect
+if ($(".odometer").length > 0) {
+  const odometerElements = document.querySelectorAll('.odometer');
 
+  odometerElements.forEach(odometer => {
+    const startValue = parseInt(odometer.getAttribute('data-start'), 10);
+    const endValue = parseInt(odometer.getAttribute('data-end'), 10);
 
-
-const odometerElements = document.querySelectorAll('.odometer');
-
-odometerElements.forEach(odometer => {
-  const startValue = parseInt(odometer.getAttribute('data-start'), 10);
-  const endValue = parseInt(odometer.getAttribute('data-end'), 10);
-
-  ScrollTrigger.create({
-    trigger: odometer,
-    start: 'top 80%',
-    onEnter: () => {
-      gsap.to({ value: startValue }, {
-        value: endValue,
-        duration: 2,
-        ease: "power2.in",
-        onUpdate: function () {
-          odometer.textContent = Math.floor(this.targets()[0].value).toLocaleString();
-        }
-      });
-    },
-    scrub: true
+    ScrollTrigger.create({
+      trigger: odometer,
+      start: 'top 80%',
+      onEnter: () => {
+        gsap.to({ value: startValue }, {
+          value: endValue,
+          duration: 2,
+          ease: "power2.in",
+          onUpdate: function () {
+            odometer.textContent = Math.floor(this.targets()[0].value).toLocaleString();
+          }
+        });
+      },
+      scrub: true
+    });
   });
-});
+}
